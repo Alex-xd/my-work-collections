ac27d4aba6c9b397c5e5aaa6fe4b7848c3879b2e
zhangboyuan-XD <zhangboyuan_xd@qq.com>
2016-11-05 01:19:25 +0800

    v1

--- vue-todoList/app.js
+++ vue-todoList/app.js
@@ -1,5 +1,33 @@
 var todolist = new Vue({
-    el: 'todolist',
-    data: ,
-    methods:
+    el: '#todolist',
+    data: {
+        newTodo: {
+            name: '',
+            deadline: ''
+        },
+        todos: [],
+        tips: ''
+    },
+    methods: {
+        addTodo: function() {
+            this.tips = '';
+            if (this.newTodo.name) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    name: '',
+                    deadline: ''
+                }
+                
+            } else {
+                this.tips = 'please enter something~';
+            }
+
+            if (!this.deadline) {
+                this.newTodo.deadline = '∞';
+            }
+        },
+        removeTodo: function(index) {
+            this.todos.splice(index, 1);
+        }
+    }
 });
